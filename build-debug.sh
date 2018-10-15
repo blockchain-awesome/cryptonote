212ad0bcef278072f32c5710e6e72f6040457783
calidion <calidion@gmail.com>
2018-10-15 14:38:26 +0800

    added codecov and build-debug.sh

---
+++ build-debug.sh
@@ -0,0 +1,11 @@
+if [ ! -d "build" ]; then
+    mkdir -p build
+fi
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_GCOV=1
+make
+env CTEST_OUTPUT_ON_FAILURE=1 make test
+lcov --capture --directory src --output-file coverage.info
+genhtml coverage.info --output-directory html
+
+xdg-open ./html/index.html
