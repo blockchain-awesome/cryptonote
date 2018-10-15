2d59cf03c527d687468997e8d6f485b290044b79
calidion <calidion@gmail.com>
2018-10-16 01:26:14 +0800

    updated codecov

--- build-debug.sh
+++ build-debug.sh
@@ -2,10 +2,14 @@ if [ ! -d "build" ]; then
     mkdir -p build
 fi
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_GCOV=1
-make
-env CTEST_OUTPUT_ON_FAILURE=1 make test
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_GCOV=1 || exit 1
+make || exit 1
+env CTEST_OUTPUT_ON_FAILURE=1 make test || exit 1
 lcov --capture --directory src --remove '/usr/include/' './include' --output-file coverage.info
-genhtml coverage.info --output-directory html
+genhtml coverage.info --output-directory html || exit 1
+
+# codecov --token 2a2cf36f-81d6-43b9-abba-d5955cb0cafe --root src
+
+bash <(curl -s https://codecov.io/bash) -t 2a2cf36f-81d6-43b9-abba-d5955cb0cafe || exit 1
 
 xdg-open ./html/index.html
