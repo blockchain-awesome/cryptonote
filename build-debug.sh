c5ffc44c428cb7287970928b0c85b515f1612bb8
calidion <calidion@gmail.com>
2018-10-15 19:41:45 +0800

    sync algorithm with monero

--- build-debug.sh
+++ build-debug.sh
@@ -5,7 +5,7 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_GCOV=1
 make
 env CTEST_OUTPUT_ON_FAILURE=1 make test
-lcov --capture --directory src --output-file coverage.info
+lcov --capture --directory src --remove '/usr/include/' './include' --output-file coverage.info
 genhtml coverage.info --output-directory html
 
 xdg-open ./html/index.html
