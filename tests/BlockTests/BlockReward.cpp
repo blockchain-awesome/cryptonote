2d2127910a97392cd24c21e307582931e3afa320
calidion <calidion@gmail.com>
2018-10-16 18:48:16 +0800

    updated some test cases

--- tests/BlockTests/BlockReward.cpp
+++ tests/BlockTests/BlockReward.cpp
@@ -52,19 +52,19 @@ namespace
     TEST_ALREADY_GENERATED_COINS(UINT64_C(2756434948434199641), 59853779316998);
   }
 
-  // TEST_F(getBlockReward_and_already_generated_coins, correctly_steps_from_reward_2_to_1) {
-  //   TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() - ((UINT64_C(2) << m_currency.emissionSpeedFactor()) + 1), 2);
-  //   TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() -  (UINT64_C(2) << m_currency.emissionSpeedFactor())     , 2);
-  //   TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() - ((UINT64_C(2) << m_currency.emissionSpeedFactor()) - 1), 1);
-  // }
+  TEST_F(getBlockReward_and_already_generated_coins, correctly_steps_from_reward_2_to_1) {
+    TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() - ((UINT64_C(2) << m_currency.emissionSpeedFactor()) + 1), 2);
+    TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() -  (UINT64_C(2) << m_currency.emissionSpeedFactor())     , 2);
+    TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() - ((UINT64_C(2) << m_currency.emissionSpeedFactor()) - 1), 1);
+  }
 
-  // TEST_F(getBlockReward_and_already_generated_coins, handles_max_already_generaged_coins) {
-  //   TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() - ((UINT64_C(1) << m_currency.emissionSpeedFactor()) + 1), 1);
-  //   TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() -  (UINT64_C(1) << m_currency.emissionSpeedFactor())     , 1);
-  //   TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() - ((UINT64_C(1) << m_currency.emissionSpeedFactor()) - 1), 0);
-  //   TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() - 1, 0);
-  //   TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply(), 0);
-  // }
+  TEST_F(getBlockReward_and_already_generated_coins, handles_max_already_generaged_coins) {
+    TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() - ((UINT64_C(1) << m_currency.emissionSpeedFactor()) + 1), 1);
+    TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() -  (UINT64_C(1) << m_currency.emissionSpeedFactor())     , 1);
+    TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() - ((UINT64_C(1) << m_currency.emissionSpeedFactor()) - 1), 0);
+    TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply() - 1, 0);
+    TEST_ALREADY_GENERATED_COINS(m_currency.moneySupply(), 0);
+  }
 
   //--------------------------------------------------------------------------------------------------------------------
   class getBlockReward_and_median_and_blockSize : public ::testing::Test {
@@ -85,7 +85,7 @@ namespace
       m_blockTooBig = !m_currency.getBlockReward(0, 0, alreadyGeneratedCoins, 0,
         m_standardBlockReward, m_emissionChange);
       ASSERT_FALSE(m_blockTooBig);
-      ASSERT_EQ(UINT64_C(70368744177663), m_standardBlockReward);
+      ASSERT_EQ(UINT64_C(1), m_standardBlockReward);
     }
 
     void do_test(size_t medianBlockSize, size_t currentBlockSize) {
@@ -101,66 +101,66 @@ namespace
     uint64_t m_standardBlockReward;
   };
 
-  // TEST_F(getBlockReward_and_median_and_blockSize, handles_zero_median) {
-  //   do_test(0, TEST_GRANTED_FULL_REWARD_ZONE);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(m_standardBlockReward, m_blockReward);
-  // }
+  TEST_F(getBlockReward_and_median_and_blockSize, handles_zero_median) {
+    do_test(0, TEST_GRANTED_FULL_REWARD_ZONE);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(m_standardBlockReward, m_blockReward);
+  }
 
-  // TEST_F(getBlockReward_and_median_and_blockSize, handles_median_lt_relevance_level) {
-  //   do_test(TEST_GRANTED_FULL_REWARD_ZONE - 1, TEST_GRANTED_FULL_REWARD_ZONE);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(m_standardBlockReward, m_blockReward);
-  // }
+  TEST_F(getBlockReward_and_median_and_blockSize, handles_median_lt_relevance_level) {
+    do_test(TEST_GRANTED_FULL_REWARD_ZONE - 1, TEST_GRANTED_FULL_REWARD_ZONE);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(m_standardBlockReward, m_blockReward);
+  }
 
-  // TEST_F(getBlockReward_and_median_and_blockSize, handles_median_eq_relevance_level) {
-  //   do_test(TEST_GRANTED_FULL_REWARD_ZONE, TEST_GRANTED_FULL_REWARD_ZONE - 1);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(m_standardBlockReward, m_blockReward);
-  // }
+  TEST_F(getBlockReward_and_median_and_blockSize, handles_median_eq_relevance_level) {
+    do_test(TEST_GRANTED_FULL_REWARD_ZONE, TEST_GRANTED_FULL_REWARD_ZONE - 1);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(m_standardBlockReward, m_blockReward);
+  }
 
-  // TEST_F(getBlockReward_and_median_and_blockSize, handles_median_gt_relevance_level) {
-  //   do_test(TEST_GRANTED_FULL_REWARD_ZONE + 1, TEST_GRANTED_FULL_REWARD_ZONE);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(m_standardBlockReward, m_blockReward);
-  // }
+  TEST_F(getBlockReward_and_median_and_blockSize, handles_median_gt_relevance_level) {
+    do_test(TEST_GRANTED_FULL_REWARD_ZONE + 1, TEST_GRANTED_FULL_REWARD_ZONE);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(m_standardBlockReward, m_blockReward);
+  }
 
-  // TEST_F(getBlockReward_and_median_and_blockSize, handles_big_median) {
-  //   size_t blockSize = 1;
-  //   size_t medianSize = std::numeric_limits<uint32_t>::max();
+  TEST_F(getBlockReward_and_median_and_blockSize, handles_big_median) {
+    size_t blockSize = 1;
+    size_t medianSize = std::numeric_limits<uint32_t>::max();
 
-  //   do_test(medianSize, blockSize);
+    do_test(medianSize, blockSize);
 
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(m_standardBlockReward, m_blockReward);
-  // }
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(m_standardBlockReward, m_blockReward);
+  }
 
-  // TEST_F(getBlockReward_and_median_and_blockSize, handles_big_block_size) {
-  //   size_t blockSize = std::numeric_limits<uint32_t>::max() - 1; // even
-  //   size_t medianSize = blockSize / 2; // 2 * medianSize == blockSize
+  TEST_F(getBlockReward_and_median_and_blockSize, handles_big_block_size) {
+    size_t blockSize = std::numeric_limits<uint32_t>::max() - 1; // even
+    size_t medianSize = blockSize / 2; // 2 * medianSize == blockSize
 
-  //   do_test(medianSize, blockSize);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(0, m_blockReward);
-  // }
+    do_test(medianSize, blockSize);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(0, m_blockReward);
+  }
 
-  // TEST_F(getBlockReward_and_median_and_blockSize, handles_big_block_size_fail) {
-  //   size_t blockSize = std::numeric_limits<uint32_t>::max();
-  //   size_t medianSize = blockSize / 2 - 1;
+  TEST_F(getBlockReward_and_median_and_blockSize, handles_big_block_size_fail) {
+    size_t blockSize = std::numeric_limits<uint32_t>::max();
+    size_t medianSize = blockSize / 2 - 1;
 
-  //   do_test(medianSize, blockSize);
-  //   ASSERT_TRUE(m_blockTooBig);
-  // }
+    do_test(medianSize, blockSize);
+    ASSERT_TRUE(m_blockTooBig);
+  }
 
-  // TEST_F(getBlockReward_and_median_and_blockSize, handles_big_median_and_block_size) {
-  //   // blockSize should be greater medianSize
-  //   size_t blockSize = std::numeric_limits<uint32_t>::max();
-  //   size_t medianSize = std::numeric_limits<uint32_t>::max() - 1;
+  TEST_F(getBlockReward_and_median_and_blockSize, handles_big_median_and_block_size) {
+    // blockSize should be greater medianSize
+    size_t blockSize = std::numeric_limits<uint32_t>::max();
+    size_t medianSize = std::numeric_limits<uint32_t>::max() - 1;
 
-  //   do_test(medianSize, blockSize);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_LT(m_blockReward, m_standardBlockReward);
-  // }
+    do_test(medianSize, blockSize);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_LT(m_blockReward, m_standardBlockReward);
+  }
 
   //--------------------------------------------------------------------------------------------------------------------
   class getBlockReward_and_currentBlockSize : public ::testing::Test {
@@ -183,7 +183,7 @@ namespace
         m_standardBlockReward, m_emissionChange);
 
       ASSERT_FALSE(m_blockTooBig);
-      ASSERT_EQ(UINT64_C(70368744177663), m_standardBlockReward);
+      ASSERT_EQ(UINT64_C(1), m_standardBlockReward);
     }
 
     void do_test(size_t currentBlockSize) {
@@ -199,29 +199,29 @@ namespace
     uint64_t m_standardBlockReward;
   };
 
-  // TEST_F(getBlockReward_and_currentBlockSize, handles_zero_block_size) {
-  //   do_test(0);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(m_standardBlockReward, m_blockReward);
-  // }
+  TEST_F(getBlockReward_and_currentBlockSize, handles_zero_block_size) {
+    do_test(0);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(m_standardBlockReward, m_blockReward);
+  }
 
-  // TEST_F(getBlockReward_and_currentBlockSize, handles_block_size_less_median) {
-  //   do_test(testMedian - 1);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(m_standardBlockReward, m_blockReward);
-  // }
+  TEST_F(getBlockReward_and_currentBlockSize, handles_block_size_less_median) {
+    do_test(testMedian - 1);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(m_standardBlockReward, m_blockReward);
+  }
 
-  // TEST_F(getBlockReward_and_currentBlockSize, handles_block_size_eq_median) {
-  //   do_test(testMedian);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(m_standardBlockReward, m_blockReward);
-  // }
+  TEST_F(getBlockReward_and_currentBlockSize, handles_block_size_eq_median) {
+    do_test(testMedian);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(m_standardBlockReward, m_blockReward);
+  }
 
-  // TEST_F(getBlockReward_and_currentBlockSize, handles_block_size_gt_median) {
-  //   do_test(testMedian + 1);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_LT(m_blockReward, m_standardBlockReward);
-  // }
+  TEST_F(getBlockReward_and_currentBlockSize, handles_block_size_gt_median) {
+    do_test(testMedian + 1);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_LT(m_blockReward, m_standardBlockReward);
+  }
 
   // TEST_F(getBlockReward_and_currentBlockSize, handles_block_size_less_2_medians) {
   //   do_test(2 * testMedian - 1);
@@ -230,35 +230,35 @@ namespace
   //   ASSERT_GT(m_blockReward, 0);
   // }
 
-  // TEST_F(getBlockReward_and_currentBlockSize, handles_block_size_eq_2_medians) {
-  //   do_test(2 * testMedian);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(0, m_blockReward);
-  // }
+  TEST_F(getBlockReward_and_currentBlockSize, handles_block_size_eq_2_medians) {
+    do_test(2 * testMedian);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(0, m_blockReward);
+  }
 
-  // TEST_F(getBlockReward_and_currentBlockSize, handles_block_size_gt_2_medians) {
-  //   do_test(2 * testMedian + 1);
-  //   ASSERT_TRUE(m_blockTooBig);
-  // }
+  TEST_F(getBlockReward_and_currentBlockSize, handles_block_size_gt_2_medians) {
+    do_test(2 * testMedian + 1);
+    ASSERT_TRUE(m_blockTooBig);
+  }
 
-  // TEST_F(getBlockReward_and_currentBlockSize, calculates_correctly) {
-  //   ASSERT_EQ(0, testMedian % 8);
+  TEST_F(getBlockReward_and_currentBlockSize, calculates_correctly) {
+    ASSERT_EQ(0, testMedian % 8);
 
-  //   // reward = 1 - (k - 1)^2
-  //   // k = 9/8 => reward = 63/64
-  //   do_test(testMedian * 9 / 8);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(m_standardBlockReward * 63 / 64, m_blockReward);
+    // reward = 1 - (k - 1)^2
+    // k = 9/8 => reward = 63/64
+    do_test(testMedian * 9 / 8);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(m_standardBlockReward * 63 / 64, m_blockReward);
 
-  //   // 3/2 = 12/8
-  //   do_test(testMedian * 3 / 2);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(m_standardBlockReward * 3 / 4, m_blockReward);
+    // 3/2 = 12/8
+    do_test(testMedian * 3 / 2);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(m_standardBlockReward * 3 / 4, m_blockReward);
 
-  //   do_test(testMedian * 15 / 8);
-  //   ASSERT_FALSE(m_blockTooBig);
-  //   ASSERT_EQ(m_standardBlockReward * 15 / 64, m_blockReward);
-  // }
+    do_test(testMedian * 15 / 8);
+    ASSERT_FALSE(m_blockTooBig);
+    ASSERT_EQ(m_standardBlockReward * 15 / 64, m_blockReward);
+  }
   //--------------------------------------------------------------------------------------------------------------------
   const unsigned int testEmissionSpeedFactor = 4;
   const size_t testGrantedFullRewardZone = 10000;
