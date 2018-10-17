fd9c92d83309bc7bf64b602fe3d7eb8754ee922a
calidion <calidion@gmail.com>
2018-10-17 21:26:36 +0800

    updated coverage script

--- build-debug.sh
+++ build-debug.sh
@@ -7,10 +7,10 @@ make || exit 1
 env CTEST_OUTPUT_ON_FAILURE=1 make test || exit 1
 # lcov --capture --directory src --remove '/usr/include/*' './include/*' --output-file coverage.info
 # lcov --remove coverage.info '/usr/include/*' './include/*' --output-file coverage1.info
-lcov --capture --directory src --output-file temp.info
-lcov --remove temp.info --output-file '/usr/include/*' './include/*' coverage.info
-lcov --list coverage.info
-genhtml coverage1.info --output-directory html || exit 1
+lcov --capture --directory src --output-file temp.info || exit 1
+lcov --remove temp.info "/usr/include/*" "./include/*" > coverage.info || exit 1
+lcov --list coverage.info || exit 1
+genhtml coverage.info --output-directory html || exit 1
 
 # codecov --token 2a2cf36f-81d6-43b9-abba-d5955cb0cafe --root src
 
