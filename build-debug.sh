65791949dd3db6eb0856c870b8dac2dc77aa4c5d
calidion <calidion@gmail.com>
2018-10-17 18:25:38 +0800

    updated coverage info

--- build-debug.sh
+++ build-debug.sh
@@ -5,7 +5,8 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_GCOV=1 || exit 1
 make || exit 1
 env CTEST_OUTPUT_ON_FAILURE=1 make test || exit 1
-lcov --capture --directory src --remove '/usr/include/' './include' --output-file coverage.info
+lcov --capture --directory src --remove '/usr/include/*' './include/*' --output-file coverage.info
+lcov --remove coverage.info '/usr/include/*' './include/*'
 genhtml coverage.info --output-directory html || exit 1
 
 # codecov --token 2a2cf36f-81d6-43b9-abba-d5955cb0cafe --root src
