aeecbc0c04ce1eb61f15c2a38ba5396cff9a78e9
calidion <calidion@gmail.com>
2018-10-25 20:32:40 +0800

    commented on mac os

--- build-debug.sh
+++ build-debug.sh
@@ -2,9 +2,11 @@ if [ ! -d "build" ]; then
     mkdir -p build
 fi
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_GCOV=1 || exit 1
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DENABLE_GCOV=1 || exit 1
 make || exit 1
 env CTEST_OUTPUT_ON_FAILURE=1 make test || exit 1
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 # lcov --capture --directory src --remove '/usr/include/*' './include/*' --output-file coverage.info
 # lcov --remove coverage.info '/usr/include/*' './include/*' --output-file coverage1.info
 lcov --capture --directory src --output-file temp.info || exit 1
@@ -17,3 +19,4 @@ genhtml coverage.info --output-directory html || exit 1
 bash <(curl -s https://codecov.io/bash) -t 2a2cf36f-81d6-43b9-abba-d5955cb0cafe || exit 1
 
 xdg-open ./html/index.html
+fi
