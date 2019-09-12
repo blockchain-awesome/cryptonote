5e3dc4cc28874b7501f0e6fc551238adffbb57ce
calidion <calidion@gmail.com>
2019-09-12 23:53:52 +0800

    removed tree_branch

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -19,13 +19,9 @@ namespace crypto {
     Cryptonight hash functions
   */
 
-  // inline void tree_hash(const hash_t *hashes, size_t count, hash_t &root_hash) {
-  //   tree_hash(reinterpret_cast<const char (*)[HASH_SIZE]>(hashes), count, reinterpret_cast<char *>(&root_hash));
+  // inline void tree_branch(const hash_t *hashes, size_t count, hash_t *branch) {
+  //   tree_branch(reinterpret_cast<const char (*)[HASH_SIZE]>(hashes), count, reinterpret_cast<char (*)[HASH_SIZE]>(branch));
   // }
-
-  inline void tree_branch(const hash_t *hashes, size_t count, hash_t *branch) {
-    tree_branch(reinterpret_cast<const char (*)[HASH_SIZE]>(hashes), count, reinterpret_cast<char (*)[HASH_SIZE]>(branch));
-  }
 
   inline void tree_hash_from_branch(const hash_t *branch, size_t depth, const hash_t &leaf, const void *path, hash_t &root_hash) {
     tree_hash_from_branch(reinterpret_cast<const char (*)[HASH_SIZE]>(branch), depth, reinterpret_cast<const char *>(&leaf), path, reinterpret_cast<char *>(&root_hash));
