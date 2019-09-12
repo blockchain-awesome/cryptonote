6ed7b7a4190d7d15a51bfef7e30272d9eaadbf07
calidion <calidion@gmail.com>
2019-09-13 00:41:31 +0800

    removed tree_hash_from_branch from C++

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -9,24 +9,14 @@
 #include <crypto.h>
 #include "generic-ops.h"
 
-namespace crypto {
+namespace crypto
+{
 
-  extern "C" {
+extern "C"
+{
 #include "hash-ops.h"
-  }
-
-  /*
-    Cryptonight hash functions
-  */
-
-  // inline void tree_branch(const hash_t *hashes, size_t count, hash_t *branch) {
-  //   tree_branch(reinterpret_cast<const char (*)[HASH_SIZE]>(hashes), count, reinterpret_cast<char (*)[HASH_SIZE]>(branch));
-  // }
-
-  inline void tree_hash_from_branch(const hash_t *branch, size_t depth, const hash_t &leaf, const void *path, hash_t &root_hash) {
-    tree_hash_from_branch(reinterpret_cast<const char (*)[HASH_SIZE]>(branch), depth, reinterpret_cast<const char *>(&leaf), path, reinterpret_cast<char *>(&root_hash));
-  }
-
 }
+
+} // namespace crypto
 
 CRYPTO_MAKE_HASHABLE(hash_t)
