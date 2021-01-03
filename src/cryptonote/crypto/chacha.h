1a28f29d8651ab654c5586f3c80c6c94cc63167f
calidion <calidion@gmail.com>
2021-01-03 15:48:33 +0800

    enabled 20.04 support

--- src/cryptonote/crypto/chacha.h
+++ src/cryptonote/crypto/chacha.h
@@ -12,15 +12,18 @@
 
 #include "hash.h"
 
-namespace crypto {
-  extern "C" {
+namespace crypto
+{
+  extern "C"
+  {
 #endif
-    void chacha8(const void* data, size_t length, const uint8_t* key, const uint8_t* iv, char* cipher);
+    void chacha8(const void *data, size_t length, const uint8_t *key, const uint8_t *iv, char *cipher);
 #if defined(__cplusplus)
   }
 
 #pragma pack(push, 1)
-  struct chacha_key_t {
+  struct chacha_key_t
+  {
     uint8_t data[CHACHA_KEY_SIZE];
 
     ~chacha_key_t()
@@ -30,22 +33,25 @@ namespace crypto {
   };
 
   // MS VC 2012 doesn't interpret `class chacha_iv_t` as POD in spite of [9.0.10], so it is a struct
-  struct chacha_iv_t {
+  struct chacha_iv_t
+  {
     uint8_t data[CHACHA_IV_SIZE];
   };
 #pragma pack(pop)
 
   static_assert(sizeof(chacha_key_t) == CHACHA_KEY_SIZE && sizeof(chacha_iv_t) == CHACHA_IV_SIZE, "Invalid structure size");
 
-  inline void chacha8(const void* data, size_t length, const chacha_key_t& key, const chacha_iv_t& iv, char* cipher) {
-    chacha8(data, length, reinterpret_cast<const uint8_t*>(&key), reinterpret_cast<const uint8_t*>(&iv), cipher);
+  inline void chacha8(const void *data, size_t length, const chacha_key_t &key, const chacha_iv_t &iv, char *cipher)
+  {
+    chacha8(data, length, reinterpret_cast<const uint8_t *>(&key), reinterpret_cast<const uint8_t *>(&iv), cipher);
   }
 
-  inline void generate_chacha_key(const std::string& password, chacha_key_t& key) {
+  inline void generate_chacha_key(const std::string &password, chacha_key_t &key)
+  {
     static_assert(sizeof(chacha_key_t) <= sizeof(hash_t), "Size of hash must be at least that of chacha_key_t");
     hash_t pwd_hash;
     cn_slow_hash(password.data(), password.size(), (char *)&pwd_hash, 0, 0);
-    memcpy(&key, &pwd_hash, sizeof(key));
+    memcpy(key.data, &pwd_hash, sizeof(key.data));
     memset(&pwd_hash, 0, sizeof(pwd_hash));
   }
 }
