9517a824818f8f74c425d65e0e1521fd660fa68b
calidion <calidion@gmail.com>
2021-04-04 11:37:41 +0800

    enabled operator for base_input_t

--- src/stream/transaction.cpp
+++ src/stream/transaction.cpp
@@ -1,9 +1,22 @@
 
 #include "transaction.h"
+
 namespace stream
 {
   namespace transaction
   {
+
+    Reader &operator>>(Reader &i, base_input_t &v)
+    {
+      i >> v.blockIndex;
+      return i;
+    }
+
+    Writer &operator<<(Writer &o, const base_input_t &v)
+    {
+      o << v.blockIndex;
+      return o;
+    }
 
     // Reader &operator>>(Reader &i, transaction_prefix_t &v)
     // {
@@ -25,6 +38,25 @@ namespace stream
     //   serializer(txP.inputs, "vin");
     //   serializer(txP.outputs, "vout");
     //   serializeAsBinary(txP.extra, "extra", serializer);
+    // }
+
+    //     void serialize(base_input_t &gen, ISerializer &serializer)
+    // {
+    //   serializer(gen.blockIndex, "height");
+    // }
+
+    // void serialize(key_input_t &key, ISerializer &serializer)
+    // {
+    //   serializer(key.amount, "amount");
+    //   serializeVarintVector(key.outputIndexes, serializer, "key_offsets");
+    //   serializer(key.keyImage, "k_image");
+    // }
+
+    // void serialize(multi_signature_input_t &multisignature, ISerializer &serializer)
+    // {
+    //   serializer(multisignature.amount, "amount");
+    //   serializer(multisignature.signatureCount, "signatures");
+    //   serializer(multisignature.outputIndex, "outputIndex");
     // }
   }
 }
--- src/stream/transaction.h
+++ src/stream/transaction.h
@@ -5,14 +5,19 @@
 #include "reader.h"
 #include "writer.h"
 
-#include "cryptonote/types.h"
+#include "cryptonote.h"
+#include "cryptonote/core/transaction/types.h"
+
+using namespace cryptonote;
 
 namespace stream
 {
   namespace cryptonote
   {
+    Reader &operator>>(Reader &i,  base_input_t &v);
     // Reader &operator>>(Reader &i, transaction_prefix_t &v);
 
+    Writer &operator<<(Writer &o, const base_input_t &v);
     // Writer &operator<<(Writer &o, const public_key_t &v);
   }
 }
